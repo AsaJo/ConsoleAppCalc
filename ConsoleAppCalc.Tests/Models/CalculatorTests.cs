713fb8fe6d8246225d74bdda93bd41151b1e9fe0
Åsa Jonsson <Asa.Jonsson@lexicon.se>
2022-09-26 11:41:07 +0200

    Add project files.

---
+++ ConsoleAppCalc.Tests/Models/CalculatorTests.cs
@@ -0,0 +1,43 @@
+using ConsoleAppCalc.Models;
+
+namespace ConsoleAppCalc.Tests.Models
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_SimpleValuesShouldCalculate()
+        {
+            // Arrange
+            double expected = 5;
+            // Act
+            double actual = Calculator.Add(3, 2);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void Divide_DivideByZero()
+        {
+            // Arrange
+            double expected = 0;
+            // Act
+            double actual = Calculator.Divide(15, 0);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(4, 3, 7)]
+        [InlineData(21, 5.25, 26.25)]
+        [InlineData(-4, 3, -1)]
+       
+        public void Add_SimpleValuesShouldCalculate2(double x, double y, double expected)
+        {
+            // Arrange
+
+            // Act
+            double actual = Calculator.Add(x, y);
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
